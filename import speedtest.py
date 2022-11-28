31372d3a38a6f1e7b1ab23fa7fd4e68bfd5a43e2
Juan C. Cabrera <107782146+JuanTheCoder2012@users.noreply.github.com>
2022-11-27 20:19:37 -0500

    Update import speedtest.py

--- import speedtest.py
+++ import speedtest.py
@@ -1,3 +1,6 @@
+# This is my first project, lets work together and improve on this and make it better.
+# Please be sure to install speedtest-cli, so you can properly use this code. 
+
 import speedtest
 
 test = speedtest.Speedtest()
